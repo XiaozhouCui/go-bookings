464eae3ff579f8066179c72fa0982cbf638491da
Joe Cui <xiaozhou.cui@gmail.com>
2022-11-05 18:38:26 +1000

    Adding html file

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -22,5 +22,6 @@ func routes(app *config.AppConfig) http.Handler {
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
