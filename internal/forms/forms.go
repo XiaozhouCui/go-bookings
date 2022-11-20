db96f2466612ed12e533c6cec2cb697d710cbb9c
Joe Cui <xiaozhou.cui@gmail.com>
2022-11-20 12:05:03 +1000

    Validate input length

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,8 +1,10 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
 
 // Form creates a custom form struct, embeds a url.Values object
@@ -25,12 +27,31 @@ func New(data url.Values) *Form {
 	}
 }
 
+// Required checks for required fields
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
 // Has checks if form field is in post and not empty
 func (f *Form) Has(field string, r *http.Request) bool {
 	// check request
 	x := r.Form.Get(field)
 	if x == "" {
-		f.Errors.Add(field, "This field cannot be blank")
+		return false
+	}
+	return true
+}
+
+// MinLength checks for string minimum length
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
 		return false
 	}
 	return true
