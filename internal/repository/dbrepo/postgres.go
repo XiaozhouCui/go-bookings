e7bd9b4e0e26c33f9bd84d2110c3d0b2ac6af987
Joe Cui <xiaozhou.cui@gmail.com>
2023-01-07 18:57:10 +1000

    Update frontend to insert reservation

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -18,7 +18,7 @@ func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 
 	stmt := `insert into reservations
 				(first_name, last_name, email, phone, start_date, end_date, room_id, created_at, updated_at)
-				values ($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+				values ($1, $2, $3, $4, $5, $6, $7, $8, $9) returning id`
 
 	_, err := m.DB.ExecContext(ctx, stmt, res.FirstName, res.LastName, res.Email, res.Phone, res.StartDate, res.EndDate, res.RoomID, time.Now(), time.Now())
 
